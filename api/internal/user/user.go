e4ba57bd06e92062fb30469a552bb291121abe1e
joseph <joebeck03@icloud.com>
2023-07-12 20:39:56 +0100

    add user handler and more to user

--- api/internal/user/user.go
+++ api/internal/user/user.go
@@ -5,12 +5,36 @@ type UserModel struct {
 	Name string
 }
 
+func (u *UserModel) ToUserPost() UserPost {
+	return UserPost{}
+}
+
+func (u *UserModel) ToUser() User {
+	return User{}
+}
+
 type UserPost struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 
+func (u *UserPost) ToUserModel() UserModel {
+	return UserModel{}
+}
+
+func (u *UserPost) ToUser() User {
+	return User{}
+}
+
 type User struct {
 	Id   int
 	Name string
+}
+
+func (u *User) ToUserModel() UserModel {
+	return UserModel{}
+}
+
+func (u *User) ToUserPost() UserPost {
+	return UserPost{}
 }
