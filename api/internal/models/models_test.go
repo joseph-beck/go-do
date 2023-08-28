ace5c655b56edbc5406859674c700220be8943a3
joseph-beck <joebeck03@icloud.com>
2023-08-28 18:06:41 +0100

    clean model tests

--- api/internal/models/models_test.go
+++ api/internal/models/models_test.go
@@ -48,3 +48,25 @@ func TestVerifyDate(t *testing.T) {
 func TestVerifyTime(t *testing.T) {
 
 }
+
+func TestTask(t *testing.T) {
+	task := Task{
+		Id:          587612,
+		Name:        "Dry",
+		Description: "dry something up",
+		Complete:    false,
+		Deadline: Deadline{
+			Date: Date{
+				Day:   "1",
+				Month: "11",
+				Year:  "2023",
+			},
+			Time: Time{
+				Hour:   "10",
+				Minute: "30",
+			},
+		},
+	}
+
+	assert.NotNil(t, task)
+}
