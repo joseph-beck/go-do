64c3ed9a0d89c590b858c46b973d63318649d68f
joseph-beck <joebeck03@icloud.com>
2023-12-21 15:50:04 +0000

    backend can now ping

--- api/internal/app/app.go
+++ api/internal/app/app.go
@@ -1,26 +1,21 @@
 package app
 
 import (
-	"fmt"
 	"go-do/internal/database"
 	"go-do/internal/router"
 	"go-do/pkg/util"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/joho/godotenv"
 )
 
 // Stores the router
-var r *router.Router
+var r router.Router
 
 // Stores the store
-var s *database.Store
+var s database.Store
 
 // Runs the app
-//
 //   - Creates the router.
 //   - Registers routes.
 //   - Runs the engine.
@@ -31,36 +26,11 @@ func Run() {
 	err := godotenv.Load()
 	util.ErrOut(err)
 
-	go shutdown()
-
 	s = database.New(database.SQLiteDb())
 
 	r = router.New()
-	r.RegisterRoutes(makeRoutes())
+	r.Register(routes)
 	r.NoRoute(reverseProxy())
+	go r.Shutdown()
 	r.Run()
-
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-	<-stop
-
-	log.Println("Shutting down app")
-}
-
-// Shuts down the app.
-//
-//   - Closes Store if not nil.
-func shutdown() {
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-
-	<-stop
-	fmt.Println("")
-	log.Println("Shutting down app")
-
-	log.Println("Closing db conn")
-	err := s.Close()
-	util.ErrLog(err)
-
-	os.Exit(0)
 }
